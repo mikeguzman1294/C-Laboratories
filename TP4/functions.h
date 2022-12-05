652af6b80e9664d07ae0b62900d924eb3a1bef74
mikeguzman1294 <mikeguzman1294@gmail.com>
2022-12-05 17:12:13 +0100

    Deleted TP4 since it is redundant against Project.

--- TP4/functions.h
+++
@@ -1,9 +0,0 @@
-int stockNameSort(const void *a, const void *b); 
-
-//Using comparison function sort the information by Stock Price (2)
-int stockPriceSort(const void *a, const void *b); 
-
-
-//Using comparison function sort the information by Stock Day (3)
-int stockDaySort(const void *a, const void *b); 
-
